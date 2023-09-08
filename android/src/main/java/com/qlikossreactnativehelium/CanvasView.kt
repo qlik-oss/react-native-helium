76c9397106aa4dd443062eaa3a7cce2500b05f11
Johan Enell <johan.enell@qlik.com>
2023-09-08 15:28:29 +0200

    feat: set target api to 33 BREAKING CHANGE: setting minimum api to 30

--- android/src/main/java/com/qlikossreactnativehelium/CanvasView.kt
+++ android/src/main/java/com/qlikossreactnativehelium/CanvasView.kt
@@ -1,18 +1,12 @@
 package com.qlikossreactnativehelium
 
 import android.content.Context
-import android.content.res.Resources
-import android.graphics.Matrix
-import android.graphics.PointF
-import android.graphics.Rect
 import android.graphics.SurfaceTexture
 import android.util.Log
 import android.view.*
-import android.widget.TextView
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.uimanager.events.RCTEventEmitter
-import com.facebook.react.uimanager.events.RCTModernEventEmitter
 
 class CanvasView : TextureView, TextureView.SurfaceTextureListener {
 
@@ -23,42 +17,37 @@ class CanvasView : TextureView, TextureView.SurfaceTextureListener {
   private var disabledSelections = false;
 
   private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
-    @Suppress("DEPRECATION")
-    override fun onSingleTapUp(e: MotionEvent?): Boolean {
-      if (e != null) {
-        if(!disabledSelections) {
-          captureView(nativeId)
-          beginSelections(e.x, e.y)
-        }
-        val reactContext = context as ReactContext
-        val event = Arguments.createMap()
-        reactContext.getJSModule(RCTEventEmitter::class.java).receiveEvent(id, "onBeganSelections", event);
+    override fun onSingleTapUp(e: MotionEvent): Boolean {
+      if (!disabledSelections) {
+        captureView(nativeId)
+        beginSelections(e.x, e.y)
       }
+      val reactContext = context as ReactContext
+      val event = Arguments.createMap()
+      reactContext.getJSModule(RCTEventEmitter::class.java)
+        .receiveEvent(id, "onBeganSelections", event);
       return false;
     }
-    
-    override fun onLongPress(e: MotionEvent?) {
-      if(e != null) {
-        captureView(nativeId)
-        val loc = IntArray(2)
-        getLocationInWindow(loc)
-        var ox = loc[0] 
-        var oy = loc[1] 
-        var rx = ox + e.x
-        var ry = oy + e.y
-        rx = pxToDp(context, rx)
-        ry = pxToDp(context, ry)
-        var x = e.x
-        var y = e.y
-         
-        handleLongPress(x, y, rx, ry)
-       
-      }
+
+    override fun onLongPress(e: MotionEvent) {
+      captureView(nativeId)
+      val loc = IntArray(2)
+      getLocationInWindow(loc)
+      var ox = loc[0]
+      var oy = loc[1]
+      var rx = ox + e.x
+      var ry = oy + e.y
+      rx = pxToDp(context, rx)
+      ry = pxToDp(context, ry)
+      var x = e.x
+      var y = e.y
+
+      handleLongPress(x, y, rx, ry)
     }
   }
 
   constructor(context: Context) : super(context) {
-    // add coment
+    // add comment
     surfaceTextureListener = this
     this.isOpaque = false
     gestureDetector = GestureDetector(context, GestureListener())
@@ -66,24 +55,29 @@ class CanvasView : TextureView, TextureView.SurfaceTextureListener {
   }
 
   override fun onTouchEvent(event: MotionEvent?): Boolean {
-    gestureDetector.onTouchEvent(event)
-    if (event != null && lasso && !disabledSelections) {
-      when (event.action) {
-        MotionEvent.ACTION_DOWN -> {
-          Log.d("MotionEVENT", "Started")
-          startLasso(event.x, event.y)
-        }
-        MotionEvent.ACTION_MOVE -> {
-          Log.d("MotionEVENT", "Moving")
-          updateLasso(event.x, event.y)
-        }
-        MotionEvent.ACTION_CANCEL -> {
-          Log.d("MotionEVENT", "Canceled")
-          endLasso(event.x, event.y)
-        }
-        MotionEvent.ACTION_UP -> {
-          Log.d("MotionEVENT", "Up")
-          endLasso(event.x, event.y)
+    if (event != null) {
+      gestureDetector.onTouchEvent(event)
+      if (lasso && !disabledSelections) {
+        when (event.action) {
+          MotionEvent.ACTION_DOWN -> {
+            Log.d("MotionEVENT", "Started")
+            startLasso(event.x, event.y)
+          }
+
+          MotionEvent.ACTION_MOVE -> {
+            Log.d("MotionEVENT", "Moving")
+            updateLasso(event.x, event.y)
+          }
+
+          MotionEvent.ACTION_CANCEL -> {
+            Log.d("MotionEVENT", "Canceled")
+            endLasso(event.x, event.y)
+          }
+
+          MotionEvent.ACTION_UP -> {
+            Log.d("MotionEVENT", "Up")
+            endLasso(event.x, event.y)
+          }
         }
       }
     }
@@ -120,7 +114,7 @@ class CanvasView : TextureView, TextureView.SurfaceTextureListener {
   fun setLasso(newVal: Boolean) {
     lasso = newVal
   }
-  
+
   fun setDisableSelections(newVal: Boolean) {
     disabledSelections = newVal;
   }
